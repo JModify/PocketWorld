de4a3a774b02ac9dd55c192c56beca4714cee35c
Josh Slater <joshua.slaterr@gmail.com>
2026-08-11 17:57:27 +1000

    Revert "Simplify: drop tagged items unconditionally, only right-click triggers wizard actions"
    This reverts commit b5e860ddd41181a8aac95a065daa8c87c46e5ab1.

--- pocketworld-plugin/src/main/java/com/pocketworld/plugin/PocketWorldPlugin.java
+++ pocketworld-plugin/src/main/java/com/pocketworld/plugin/PocketWorldPlugin.java
@@ -48,6 +48,7 @@ public final class PocketWorldPlugin extends JavaPlugin {
     private MessageReader messageReader;
     private PocketDebugger debugger;
     private ChatInputRegistry chatInputRegistry;
+    private ProtectedItemListener protectedItemListener;
 
     private PocketWorldRuntime runtime;
     private PocketWorldRuntime themeRuntime;
@@ -102,8 +103,9 @@ public final class PocketWorldPlugin extends JavaPlugin {
         themeRegistry.load();
 
         WorldAutoUnloadTracker autoUnloadTracker = new WorldAutoUnloadTracker(this);
+        protectedItemListener = new ProtectedItemListener(this);
         getServer().getPluginManager().registerEvents(new ThemeCreationListener(this), this);
-        getServer().getPluginManager().registerEvents(new ProtectedItemListener(this), this);
+        getServer().getPluginManager().registerEvents(protectedItemListener, this);
         getServer().getPluginManager().registerEvents(new PlayerListener(this, autoUnloadTracker), this);
         getServer().getPluginManager().registerEvents(new WorldListener(this), this);
         getServer().getPluginManager().registerEvents(new InventoryListener(), this);
@@ -212,6 +214,10 @@ public final class PocketWorldPlugin extends JavaPlugin {
 
     public ChatInputRegistry getChatInputRegistry() {
         return chatInputRegistry;
+    }
+
+    public ProtectedItemListener getProtectedItemListener() {
+        return protectedItemListener;
     }
 
     public PocketWorldRuntime getRuntime() {
